8cf52abd1f5556cb2941c5ef371dad3f8f41853e
Good'n Paul <73657550+Goodn6138@users.noreply.github.com>
2026-05-28 13:15:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
+import os
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# store latest sensor data
 data = {
     "height": 0,
     "weight": 0
 }
 
-# homepage
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Arduino sends data here
 @app.route("/update", methods=["POST"])
 def update():
     global data
@@ -24,10 +22,11 @@ def update():
 
     return jsonify({"status": "ok"})
 
-# frontend fetches data here
 @app.route("/data")
 def get_data():
     return jsonify(data)
 
+# REQUIRED for Render
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
